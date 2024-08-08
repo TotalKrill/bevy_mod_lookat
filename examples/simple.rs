eb67ae8d455bd22fc3b06444a86057cd539705c9
Kristoffer Ödmark <kristoffer.odmark90@gmail.com>
2024-08-08 20:38:30 +0200

    more options, and also expose the math function for this rotation

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,6 +1,6 @@
 //! A simple 3D scene with light shining over a cube sitting on a plane.
 
-use bevy::{color::palettes::css::*, prelude::*};
+use bevy::{color::palettes::css::*, prelude::*, render::primitives::Aabb};
 use bevy_mod_lookat::*;
 
 fn main() {
@@ -8,17 +8,8 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .add_plugins(RotateTowardsPlugin)
         .add_systems(Startup, setup)
-        .add_systems(Update, (move_entities, rotate))
+        .add_systems(Update, (mover, rotate, draw_axes, draw_forward))
         .run();
-}
-
-fn move_entities(time: Res<Time>, mut ents: Query<&mut Transform, With<Move>>) {
-    let distance = 2.0;
-    for mut ent in ents.iter_mut() {
-        ent.translation.x = distance * f32::sin(time.elapsed().as_secs_f32());
-        ent.translation.z = distance * f32::cos(time.elapsed().as_secs_f32());
-        ent.translation.y = 0.5 * distance * f32::cos(3.0 * time.elapsed().as_secs_f32());
-    }
 }
 
 #[derive(Component)]
@@ -27,10 +18,43 @@ struct Move;
 #[derive(Component)]
 struct Rotate;
 
+#[derive(Component)]
+struct ShowAxes;
+
+#[derive(Component)]
+struct ShowForward;
+
+fn draw_axes(mut gizmos: Gizmos, query: Query<(&GlobalTransform, &Aabb), With<ShowAxes>>) {
+    for (&transform, &aabb) in &query {
+        let t = transform.compute_transform();
+
+        let length = aabb.half_extents.length();
+        gizmos.axes(t, length);
+    }
+}
+fn draw_forward(mut gizmos: Gizmos, query: Query<&GlobalTransform, With<ShowForward>>) {
+    for &transform in &query {
+        let t = transform.compute_transform();
+
+        gizmos.line(
+            t.translation,
+            t.translation + t.forward() * 6.0,
+            Color::BLACK,
+        );
+    }
+}
 fn rotate(mut query: Query<&mut Transform, With<Rotate>>, time: Res<Time>) {
     for mut transform in &mut query {
         transform.rotate_y(time.delta_seconds() / 2.);
         transform.rotate_x(time.delta_seconds());
+    }
+}
+fn mover(time: Res<Time>, mut ents: Query<&mut Transform, With<Move>>) {
+    let distance = 2.0;
+    for mut ent in ents.iter_mut() {
+        ent.translation.x = distance * f32::sin(time.elapsed().as_secs_f32());
+        ent.translation.z = distance * f32::cos(time.elapsed().as_secs_f32());
+        ent.translation.y = 1.5 + 0.5 * distance * f32::cos(3.0 * time.elapsed().as_secs_f32());
     }
 }
 /// set up a simple 3D scene
@@ -55,17 +79,22 @@ fn setup(
                 ..default()
             },
             Move,
+            Rotate,
+            ShowAxes,
         ))
         .id();
 
     // cube
     commands
-        .spawn(PbrBundle {
-            mesh: meshes.add(Cuboid::new(1.0, 1.0, 1.0)),
-            material: materials.add(Color::srgb_u8(124, 144, 255)),
-            transform: Transform::from_xyz(0.0, 0.5, 0.0),
-            ..default()
-        })
+        .spawn((
+            PbrBundle {
+                mesh: meshes.add(Cuboid::new(1.0, 1.0, 1.0)),
+                material: materials.add(Color::srgb_u8(124, 144, 255)),
+                transform: Transform::from_xyz(2.0, 0.5, 0.0),
+                ..default()
+            },
+            ShowAxes,
+        ))
         .with_children(|commands| {
             commands
                 .spawn((
@@ -76,17 +105,22 @@ fn setup(
                         transform: Transform::from_xyz(0.0, 0.5, 0.0),
                         ..default()
                     },
+                    ShowAxes,
                 ))
                 .with_children(|parent| {
                     parent.spawn((
                         PbrBundle {
-                            mesh: meshes.add(Cuboid::new(0.1, 0.1, 2.0)),
+                            mesh: meshes.add(Cuboid::new(0.3, 0.1, 2.0)),
                             material: materials.add(Color::from(GREEN)),
                             transform: Transform::from_xyz(0.0, 0.7, 0.0),
                             ..default()
                         },
-                        // makes this component rotate towards the set target
-                        RotateTo(target_id),
+                        RotateTo {
+                            entity: target_id,
+                            // this choses what the flat side should be in relation towards
+                            updir: UpDirection::Parent,
+                        },
+                        ShowForward,
                     ));
                 });
         });
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,31 @@
 use bevy::{prelude::*, transform::systems::propagate_transforms};
 
 #[derive(Component)]
-pub struct RotateTo(pub Entity);
+/// When this component is added on an entity, [`Transform::forward()`] direction points towards the selected
+/// entity
+pub struct RotateTo {
+    /// entity to target, the Targeted entity must have a [`GlobalTransform`]
+    pub entity: Entity,
+    /// The rotated entity will match its [`Transform::up()`] according to this
+    pub updir: UpDirection,
+}
 
+/// The rotated entity will try to have its [`Transform::up()`] direction matching this selection
+pub enum UpDirection {
+    /// Will synchronize the direction of UP towards the UP direction of the target
+    /// Useful when rotating towards the camera and wanting the direction to be up for example
+    Target,
+    /// Keeps the up-direction the same as for the parent of this entity
+    /// useful when you want it rotated in relation to what this entity is attached to
+    /// Note: if there is no parent, the up direction will fallback to be Vec3::Y
+    Parent,
+    /// Keeps a static direction of UP set to this value
+    /// useful when you want to decide what is up for the entity under rotation
+    Dir(Dir3),
+}
+
+/// Plugin that constantly rotates entities towards a selected target when they have the [`RotateTo`] component on them
+/// if you only want the math for calculating the local rotation needed to look at a target, see [`calculate_local_rotation_to_target`]
 pub struct RotateTowardsPlugin;
 
 impl Plugin for RotateTowardsPlugin {
@@ -13,24 +36,56 @@ impl Plugin for RotateTowardsPlugin {
 
 fn rotate_towards(
     global_transforms: Query<&GlobalTransform>, // potential_targets
-    mut query: Query<(&mut Transform, &GlobalTransform, Option<&Parent>, &RotateTo)>, // the ones to rotate
+    mut rotators: Query<(&mut Transform, &GlobalTransform, Option<&Parent>, &RotateTo)>, // the ones to rotate
 ) {
-    for (mut rotated_t, rotated_gt, parent, target) in query.iter_mut() {
-        let Ok(target_gt) = global_transforms.get(target.0) else {
-            bevy::log::error!("Entity used as target was not found: {}", target.0);
+    for (mut rotator_t, rotator_gt, parent, target) in rotators.iter_mut() {
+        let Ok(target_gt) = global_transforms.get(target.entity) else {
+            bevy::log::error!("Entity used as target was not found: {}", target.entity);
             continue;
         };
-        let target_gt = target_gt.compute_transform();
-        let mut rotation = rotated_gt
-            .compute_transform()
-            .looking_at(target_gt.translation, target_gt.up())
-            .rotation;
 
-        if let Some(parent_e) = parent {
-            if let Ok(parent_gt) = global_transforms.get(parent_e.get()) {
-                rotation = parent_gt.compute_transform().rotation.inverse() * rotation;
+        let parent_gt = if let Some(parent_e) = parent {
+            global_transforms.get(parent_e.get()).ok()
+        } else {
+            None
+        };
+
+        let updir = match target.updir {
+            UpDirection::Target => target_gt.up(),
+            UpDirection::Dir(dir) => dir,
+            UpDirection::Parent => {
+                if let Some(parent_gt) = parent_gt {
+                    parent_gt.up()
+                } else {
+                    // if there is no parent, fallback to bevy up direction
+                    Dir3::Y
+                }
             }
         };
-        rotated_t.rotation = rotation;
+
+        let rotation = calculate_local_rotation_to_target(rotator_gt, target_gt, parent_gt, updir);
+
+        rotator_t.rotation = rotation;
     }
+}
+
+/// Calculates the local rotation on a rotator towards a target, adjusting for rotations of eventual parents, with the selected rotator up direction.
+pub fn calculate_local_rotation_to_target(
+    rotator_gt: &GlobalTransform,
+    target_gt: &GlobalTransform,
+    parent_gt: Option<&GlobalTransform>,
+    updir: Dir3,
+) -> Quat {
+    let target_gt_computed = target_gt.compute_transform();
+    let parent_gt_computed: Option<Transform> = parent_gt.map(|p| p.compute_transform());
+
+    let mut rotation = rotator_gt
+        .compute_transform()
+        .looking_at(target_gt_computed.translation, updir)
+        .rotation;
+
+    if let Some(parent_gt_computed) = parent_gt_computed {
+        rotation = parent_gt_computed.rotation.inverse() * rotation;
+    }
+    rotation
 }
